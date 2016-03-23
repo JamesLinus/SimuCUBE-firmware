a10ff0e2df07c919e33d28d9d36fab86e981ff94
Tero Kontkanen <tero.kontkanen@granitedevices.fi>
2016-03-23 18:08:47 +0200

    1) Implement SM bus takeover/give function 2) Some ADC testing code added

--- main.cpp
+++ main.cpp
@@ -42,9 +42,11 @@ int main(void) {
 
 #include "USBMouse.h"
 //#include "USBKeyboard.h"
+#include "stm32f4xx_hal_gpio.h"
 
 Serial SMSerial(PB_10, PB_11); // tx, rx
 DigitalOut SMSerialTXEN(PD_8);
+bool SMSerialMasterIsMe=true;
 
 #ifdef __cplusplus
 extern "C" {
@@ -53,6 +55,12 @@ extern "C" {
 int SMPortWrite(const char *data, int len)
 {
 	int i;
+
+	//if we are not in control of SM bus, return
+	if(SMSerialMasterIsMe==false)
+		return 0;
+
+	//write
 	SMSerialTXEN=1;
 	for(i=0;i<len;i++)
 		SMSerial.putc(data[i]);
@@ -68,6 +76,11 @@ int SMPortReadByte( char *byte )
 	timeout.start();
 	timeout.reset();
 
+	//if we are not in control of SM bus, return
+	if(SMSerialMasterIsMe==false)
+		return 0;
+
+	//try reading a byte
 	bool done=false;
 	do
 	{
@@ -86,7 +99,39 @@ int SMPortReadByte( char *byte )
 }
 #endif
 
+//takes control of SM bus for IONI if parameter is true, if false, lets second USB port (dedicated Granity port) to control it
+//in practice it turns TX & TXEN to high impedance state (inputs) when me=false and outputs when true
+void SMPortSetMaster(bool me)
+{
+	GPIO_InitTypeDef pin;
+
+	//TX
+	pin.Pin=GPIO_PIN_10;
+	pin.Speed=GPIO_SPEED_FREQ_LOW;
+	pin.Pull=GPIO_NOPULL;
+	pin.Alternate=7;//USART3
+	if(me)
+		pin.Mode=GPIO_MODE_AF_PP;
+	else
+		pin.Mode=GPIO_MODE_INPUT;
+	HAL_GPIO_Init(GPIOB,&pin);
+
+	//TXEN
+	pin.Pin=GPIO_PIN_8;
+	if(me)
+		pin.Mode=GPIO_MODE_OUTPUT_PP;
+	else
+		pin.Mode=GPIO_MODE_INPUT;
+	HAL_GPIO_Init(GPIOD,&pin);
+
+	SMSerialMasterIsMe=me;
+}
+
+
 USBMouse mouse;
+AnalogIn ADCUpperPin3(PC_5);
+AnalogIn ADCUpperPin2(PB_0);
+AnalogIn ADCUpperPin1(PB_1);
 //USBKeyboard keyboard;//keyb and mouse dont work simultaneously. hang in init
 
 Serial pc(PA_9, PA_10); // tx, rx
@@ -98,28 +143,44 @@ int main() {
 	SMSerial.baud(460800);
 	pc.baud(230400);
 
+	Timer t;
+	t.start();
+	int triggered=0;
+	int cnt=0;
+DigitalOut test(PB_5);
+test=1;
+
     while(1) {
         //mouse.move(1, 1);     //moves the mouse down and to the left
         //keyboard.keyCode('s');
 
         led1 = !led1;     //cycles the LED on/off
-        //wait(0.15);        //waits 2 seconds, then repeats
+        wait(0.05);        //waits 2 seconds, then repeats
 
-        //simplemotion.printf("fdasfsadfasdfasdfsadfas");
-        //SMPortWrite("fdsafdsafdsafdsafdsa",5);
+        if(cnt&32)
+        	test=1;
+        else
+        	test=0;
 
         SM_STATUS stat=smSetParameter(h,1,SMP_ABSOLUTE_SETPOINT,1111);
-//        pc.printf("write stat %d\n",stat);
         smint32 read;
         stat|=smRead1Parameter(h,1,SMP_ACTUAL_BUS_VOLTAGE,&read);
-  //      pc.printf("read stat %d\n",stat);
-        pc.printf("val %d, stat %d\n",read,stat);
+        pc.printf("val %d; stat %d; %f; %f; %f\n",read,stat,ADCUpperPin1.read(),ADCUpperPin2.read(),ADCUpperPin3.read());
         stat=0;
 
-        /*if(pc.readable()) {
-            pc.putc(pc.getc());
-        }*/
 
+        /*if(t.read()>20 && triggered==0)
+        {
+        	SMPortSetMaster(false);
+        	triggered=1;
+        }
+
+        if(t.read()>60 && triggered==1)
+        {
+        	SMPortSetMaster(true);
+        	triggered=2;
+        	pc.printf("MASTER\n");
+        }*/
     }
 }
 
